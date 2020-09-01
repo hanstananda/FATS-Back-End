531dc65a778771fca41ea12ee2f6f264bb8eb8de
Hans Tananda <hanstananda@gmail.com>
2020-09-01 18:37:28 +0800

    added stringify for some models

--- attendance_server/models.py
+++ attendance_server/models.py
@@ -9,11 +9,22 @@ class Course(models.Model):
     index = models.CharField(max_length=200, blank=True, null=True)
     type = models.CharField(max_length=200, blank=True, null=True)
 
+    def __str__(self):
+        res = self.name
+        if self.index:
+            res += " index: %s" % self.index
+        if self.type:
+            res += " type: %s" % self.type
+        return res
+
 
 class CourseSchedule(models.Model):
     name = models.ForeignKey(Course, on_delete=models.CASCADE)
     time = models.DateTimeField()
     teachers = models.ManyToManyField(User)
+
+    def __str__(self):
+        return str(self.name) + " at %s" % self.time.isoformat()
 
 
 class Attendance(models.Model):

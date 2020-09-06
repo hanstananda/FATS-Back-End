94d0ba39af97a476443caee93eaeff0fc66a923e
Hans Tananda <hanstananda@gmail.com>
2020-09-06 14:31:48 +0800

    added endpoints to get course and courseclass from teacher view

--- attendance_server/permissions.py
+++ attendance_server/permissions.py
@@ -2,19 +2,31 @@ import logging
 
 from rest_framework import permissions
 
-from attendance_server.models import TeacherProfile
+from attendance_server.models import TeacherProfile, CourseClass, CourseTeacher
 
 
-class TeacherOwnerOnly(permissions.BasePermission):
+class CourseTeacherOwnerOnly(permissions.BasePermission):
     """
     Object-level permission to only allow owners of an object to edit it.
-    Assumes the model instance has an `owner` attribute.
+    Assumes the model instance has an `teachers` attribute.
     """
 
     def has_object_permission(self, request, view, obj):
         # Instance must have an attribute named `teachers`.
         if request.method in permissions.SAFE_METHODS:
             return obj.teachers.user == request.user
+        return False
+
+
+class CourseClassOwnerOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj: CourseClass):
+        # Instance must have an attribute named `teachers`.
+        if request.method in permissions.SAFE_METHODS:
+            return True
         return False
 
 

99656868ec43070cd2dedf520af6c1ad5bafcdd4
Hans Tananda <hanstananda@gmail.com>
2020-09-07 22:39:44 +0800

    updated test case

--- tests/test_django_login.py
+++ tests/test_django_login.py
@@ -1,14 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
+
 from attendance_server.models import TeacherProfile
 from django.contrib.auth.models import User
-
+from rest_framework.test import APIClient
+from rest_framework import status
 
 class TeacherLoginTestCase(TestCase):
+    USERNAME = "teacher"
+    PASSWORD = "teacher"
+
     def setUp(self):
-        user = User.objects.create_user(username="admin", password="admin")
+        user = User.objects.create_user(username=self.USERNAME, password=self.PASSWORD)
         TeacherProfile.objects.create(user=user)
 
     def test_model_created(self):
-        teacher = TeacherProfile.objects.filter(user=User.objects.all().first())
-        assert(teacher.exists())
+        user = User.objects.filter(username=self.USERNAME).first()
+        teacher = TeacherProfile.objects.filter(user=user)
+        assert (teacher.exists())
 
+    def test_view(self):
+        client = APIClient()
+        response = client.post(reverse('teacher-login'), {'username': self.USERNAME, 'password': self.PASSWORD}, format='json')
+        assert (response.status_code == status.HTTP_200_OK)
